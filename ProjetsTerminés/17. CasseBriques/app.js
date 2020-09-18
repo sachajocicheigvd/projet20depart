66d5f6ca1a12d74043bf1f508bbc5ccbe000d965
Ustariz Enzo <enzo.ustariz@hotmail.fr>
2020-09-18 16:11:53 +0200

    Update app.js

--- ProjetsTerminés/17. CasseBriques/app.js
+++ ProjetsTerminés/17. CasseBriques/app.js
@@ -1,85 +1,181 @@
-const info = document.querySelector('.info');
-const cellules = document.querySelectorAll('.cell');
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const affichageScore = document.querySelector('.score');
 
-let verouillage = true;
-let joueurEnCours = "X";
+const rayonBalle = 10, barreHeight = 10, barreWidth =75,
+nbCol = 8, nbRow = 5, largeurBrique = 75, hauteurBrique = 20;
 
-info.innerHTML = `Au tour de ${joueurEnCours}`;
+let x = canvas.width/2, y = canvas.height -30,
+barreX = (canvas.width - barreWidth)/2, fin = false,
+vitesseX = 5, vitesseY = -5, score = 0;
 
-const alignementsGagnants = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-]
+function dessineBalle(){
 
-let partieEnCours = ["","","","","","","","",""];
+    ctx.beginPath();
+    ctx.arc(x,y, rayonBalle, 0, Math.PI*2);
+    ctx.fillStyle = '#333';
+    ctx.fill();
+    ctx.closePath();
 
-cellules.forEach(cell => {
-    cell.addEventListener('click', clicSurCase);
+}
+// dessineBalle();
+
+
+function dessineBarre(){
+
+    ctx.beginPath();
+    ctx.rect(barreX, canvas.height - barreHeight - 2, barreWidth, barreHeight)
+    ctx.fillStyle = '#333';
+    ctx.fill();
+    ctx.closePath();
+
+}
+// dessineBarre();
+
+// Tableau avec toutes les briques
+const briques = [];
+for(let i = 0; i < nbRow; i++){
+
+    briques[i] = [];
+
+    for(let j = 0; j < nbCol; j++){
+
+        briques[i][j] = {x: 0, y: 0, statut: 1}
+
+    }
+
+}
+// console.log(briques);
+
+function dessineBriques(){
+
+    for(let i = 0; i < nbRow; i++){
+        for(let j = 0; j < nbCol; j++){
+
+            if(briques[i][j].statut === 1){
+
+                // 75 * 8 + 10 * 8 + 35 = 750
+                let briqueX = (j * (largeurBrique + 10) + 35);
+                let briqueY = (i * (hauteurBrique + 10) + 30);
+
+                briques[i][j].x = briqueX;
+                briques[i][j].y = briqueY;
+
+                ctx.beginPath();
+                ctx.rect(briqueX, briqueY, largeurBrique, hauteurBrique);
+                ctx.fillStyle = '#333';
+                ctx.fill();
+                ctx.closePath();
+            }
+
+        }
+    }
+
+}
+// dessineBriques();
+
+
+function dessine(){
+
+    if(fin === false){
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        dessineBriques();
+        dessineBalle();
+        dessineBarre();
+        collisionDetection();
+
+        if(x + vitesseX > canvas.width - rayonBalle || x + vitesseX < rayonBalle){
+            vitesseX = -vitesseX;
+        }
+        
+        if(y + vitesseY < rayonBalle){
+            vitesseY = -vitesseY;
+        }
+
+        if(y + vitesseY > canvas.height - rayonBalle){
+
+            // un intervalle
+            // 0 - 75
+            if(x > barreX && x < barreX + barreWidth){
+                vitesseX = vitesseX + 0.1;
+                vitesseY = vitesseY + 0.1;
+                vitesseY = -vitesseY;
+            }
+            else {
+                fin = true;
+                affichageScore.innerHTML = `Perdu ! <br> Clique sur le casse-briques pour recommencer.`
+            }
+    }
+
+
+        x += vitesseX;
+        y += vitesseY;
+        requestAnimationFrame(dessine);
+
+    }
+
+}
+dessine();
+
+function collisionDetection(){
+
+    for(let i = 0; i < nbRow; i++){
+        for(let j = 0; j < nbCol; j++){
+
+            let b = briques[i][j];
+            if(b.statut === 1){
+                if(x > b.x && x < b.x + largeurBrique && y > b.y && y < b.y + hauteurBrique){
+                    vitesseY = -vitesseY;
+                    b.statut = 0;
+
+                    score++;
+                    affichageScore.innerHTML = `Score : ${score}`;
+
+                    if(score === nbCol * nbRow){
+
+                        affichageScore.innerHTML = `Bravo ! <br> Clique sur le casse-briques pour recommencer.`
+                        fin = true;
+
+                    }
+                }
+            }
+
+        }
+    }
+
+}
+
+
+
+// Mouvement de la barre
+
+document.addEventListener('mousemove', mouvementSouris);
+
+function mouvementSouris(e){
+
+    let posXBarreCanvas = e.clientX - canvas.offsetLeft;
+    // e.clienX = de la gauche jusqu'à la souris
+    // canvas.offsetLeft = décalage par rapport à la gauche
+    // console.log(posXBarreCanvas);
+
+    if(posXBarreCanvas > 35 && posXBarreCanvas < canvas.width - 35){
+        barreX = posXBarreCanvas - barreWidth/2;
+    }
+
+}
+
+
+
+
+
+// Recommencer
+canvas.addEventListener('click', () => {
+
+    if(fin === true){
+        fin = false;
+        document.location.reload();
+    }
+
 })
 
-function clicSurCase(e){
-    const caseClique = e.target;
-    const caseIndex = caseClique.getAttribute('data-index');
-
-    if(partieEnCours[caseIndex] !== "" || !verouillage){
-        return;
-    }
-
-    partieEnCours[caseIndex] = joueurEnCours;
-    caseClique.innerHTML = joueurEnCours;
-    console.log(partieEnCours)
-
-    validationResultats();
-
-}
-
-function validationResultats(){
-
-    let finDePartie = false;
-
-    for(let i = 0; i < alignementsGagnants.length; i++ ){
-
-        const checkWin = alignementsGagnants[i];
-        //  [0, 1, 2],
-        let a = partieEnCours[checkWin[0]];
-        let b = partieEnCours[checkWin[1]];
-        let c = partieEnCours[checkWin[2]];
-
-        if(a === '' || b === '' || c === ''){
-            continue;
-        }
-        if( a === b && b === c){
-            finDePartie = true;
-            break;
-        }
-    }
-
-    if(finDePartie){
-        info.innerText = `Le joueur ${joueurEnCours} a gagné !`
-        verouillage = false;
-        return;
-    }
-
-    // si il n'y pas de chaine de caracteres vides dans partie en cours
-    let matchNul = !partieEnCours.includes('');
-    if(matchNul){
-        info.innerText =  'Match nul !';
-        verouillage = false;
-        return;
-    }
-
-    changementDeJoueur();
-}
-
-
-
-function changementDeJoueur(){
-    joueurEnCours = joueurEnCours === "X" ? "O" : "X";
-    info.innerText = `Au tour de ${joueurEnCours}`;
-}
